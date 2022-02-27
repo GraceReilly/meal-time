12b82f722be92a2666664762a2c335db6090bece
GraceReilly <97609887+GraceReilly@users.noreply.github.com>
2022-02-27 19:21:22 +0000

    Update app.py

--- Mealtime/app.py
+++ Mealtime/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='template')
 
-@app.route('/login')
+@app.route('/')
 def login():
     return render_template("login.html", tag="login")
 
