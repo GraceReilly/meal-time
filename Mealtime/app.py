4b201925ac859be33c4b7877d0a5719b24dce9a6
GraceReilly <97609887+GraceReilly@users.noreply.github.com>
2022-03-08 18:01:48 +0000

    Add files via upload

--- Mealtime/app.py
+++ Mealtime/app.py
@@ -1,23 +1,63 @@
 #Template inheritance, CSS exercise
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
 
 app = Flask(__name__, template_folder='template')
 
-@app.route('/')
-def login():
-    return render_template("login.html", tag="login")
+#For Workbench use 'localhost' 
+#For editor.computing use 'student.computing.dcu.ie'
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='aliceinwonderlanD1!'
+app.config['MYSQL_DATABASE_DB']='mealtime'
 
-@app.route('/search')
-def search():
-    return render_template("Search.html", tag="search")
+mysql = MySQL(app)
 
-@app.route('/recipes')
+#This function will retrieve all data from database
+@app.route('/Recipes', methods=['GET', 'POST'])
 def recipes():
-    return render_template("recipes.html", tag="recipes")
+    
+    cur = mysql.get_db().cursor()
+    query1 = "SELECT * FROM mealtime.recipe"
+    cur.execute(query1)
+    output = cur.fetchall()
+    mysql.get_db().commit()
+    cur.close()
+    
+    return render_template('recipes.html', person = output)
 
-@app.route('/socials')
+@app.route('/Search', methods=['GET', 'POST'])
+def search():
+    
+    if request.method == 'POST':
+        ingredient = request.form['ingredient']
+        cur = mysql.get_db().cursor()
+        query1 = "SELECT * FROM mealtime.recipe WHERE recipe_ingredient=%s"
+        cur.execute(query1, (recipe_ingredients,))
+        output = cur.fetchall()
+        mysql.get_db().commit()
+        cur.close()
+        return render_template('filter_result.html', recipe_ingredient = output)
+    
+    return render_template('Search.html', recipe_ingredient = output)
+    
+
+#This function will add a new person data (except photo)
+@app.route('/socials', methods=['GET', 'POST'])
 def socials():
-    return render_template("socials.html", tag="socials")
+    
+    if request.method == 'POST':
+        name = request.form['namef']
+        ingredient = request.form['ingredientf']
+        instructions = request.form['instructionsf']
+        cur = mysql.get_db().cursor()
+        query1 = "INSERT INTO mealtime.e8_table (recipename, recipeingredient, recipeinstructions, recipetime, recipeimage) VALUES (%s, %s, %s,%s, %s)" 
+        cur.execute(query1, (recipe_name, recipe_ingredient, recipe_description, recipe_image, recipe_time)
+        mysql.get_db().commit()
+        cur.close()
+        return "Successfully added, thank you."
+    
+    return render_template('socials.html', tag="socials")
 
 #Use if in localhost environment
 if __name__=='__main__':
